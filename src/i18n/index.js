db84fc82761ada99968f06968289ff730db5bc52
dusaisai <dusaisai@miaozhen.com>
2019-09-05 15:39:00 +0800

    *）element vue-i18n导致DatePicker日期控件错乱，是因为没有引入 element-ui 语言包，引入 相应的语言包即可

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,14 +6,26 @@ import VueI18n from 'vue-i18n'
 import locale from 'element-ui/lib/locale' // 兼容 element-ui 语言
 import zh from './lang/zh'
 import en from './lang/en'
+
+import enLocale from 'element-ui/lib/locale/lang/en' //element ui 自带语言文件
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+
 Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    ...en,
+    ...enLocale // 或者用 Object.assign({ message: 'hello' }, enLocale)
+  },
+  zh: {
+    ...zh,
+    ...zhLocale // 或者用 Object.assign({ message: '你好' }, zhLocale)
+  }
+}
 
 const i18n = new VueI18n({
   locale: localStorage.getItem('locale') || 'zh', // 语言标识
-  messages: {
-    zh,
-    en
-  }
+  messages: messages
 /* require 用法 使用，以及文件格式
  messages: {
     zh: require('./lang/zh.json'),
@@ -22,5 +34,5 @@ const i18n = new VueI18n({
 })
 
 // 按需加载里定制 i18n  详细查看 elementui 官网
-locale.i18n((key, value) => i18n.t(key, value))
+//locale.i18n((key, value) => i18n.t(key, value))
 export default i18n
